5cd974aec298e9e1871b3b959111eebdae36a747
tic <tic@openengine.dk>
2007-10-23 18:22:23 +0000

    OERacer basecode

---
+++ GameFactory.cpp
@@ -0,0 +1,262 @@
+// Game Factory for the OERacer project.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+// Class header
+#include "GameFactory.h"
+
+// OpenEngine library
+#include <Display/FollowCamera.h>
+#include <Display/Frustum.h>
+#include <Display/InterpolatedViewingVolume.h>
+#include <Display/ViewingVolume.h>
+#include <Display/SDLFrame.h>
+#include <Devices/SDLInput.h>
+#include <Logging/Logger.h>
+#include <Renderers/IRenderNode.h>
+#include <Renderers/RenderStateNode.h>
+#include <Renderers/OpenGL/Renderer.h>
+#include <Renderers/OpenGL/RenderingView.h>
+#include <Resources/IModelResource.h>
+#include <Resources/File.h>
+#include <Resources/GLSLResource.h>
+#include <Resources/TGAResource.h>
+#include <Resources/OBJResource.h>
+#include <Resources/ResourceManager.h>
+#include <Scene/SceneNode.h>
+#include <Scene/GeometryNode.h>
+#include <Scene/TransformationNode.h>
+#include <Utils/Statistics.h>
+
+// from AccelerationStructures extension
+#include <Scene/QuadTreeBuilder.h>
+#include <Scene/QuadBSPBuilder.h>
+#include <Scene/BSPTreeBuilder.h>
+#include <Renderers/AcceleratedRenderingView.h>
+
+// from FixedTimeStepPhysics
+#include <Physics/FixedTimeStepPhysics.h>
+#include <Physics/RigidBox.h>
+
+// from GenericHandler extension
+#include <Utils/RenderStateHandler.h>
+#include <Utils/MoveHandler.h>
+#include <Utils/QuitHandler.h>
+
+// Project files
+#include "KeyboardHandler.h"
+
+// Additional namespaces (others are in the header).
+using namespace OpenEngine::Devices;
+using namespace OpenEngine::Renderers::OpenGL;
+using namespace OpenEngine::Renderers;
+using namespace OpenEngine::Resources;
+using namespace OpenEngine::Utils;
+using namespace OpenEngine::Physics;
+
+// composite rendering view. Uses RenderingView for drawing and
+// AcceleratedRenderingView for clipping. 
+class MyRenderingView : 
+    public RenderingView,
+    public AcceleratedRenderingView {
+public:
+    MyRenderingView(Viewport& viewport) :
+        IRenderingView(viewport),
+        RenderingView(viewport),
+        AcceleratedRenderingView(viewport) {
+
+    }
+};
+
+/**
+ * Factory constructor.
+ *
+ * Initializes the different components so they are accessible when
+ * the setup method is executed.
+ */
+GameFactory::GameFactory() {
+
+    // Create a frame and viewport.
+    this->frame = new SDLFrame(800, 600, 32);
+
+    // Main viewport
+    viewport = new Viewport(*frame);
+
+    // Create a renderer.
+    this->renderer = new Renderer();
+
+    // Add a rendering view to the renderer
+    this->renderer->AddRenderingView(new MyRenderingView(*viewport));
+}
+
+/**
+ * Setup handler.
+ *
+ * This is the main setup method of the game factory. Here you can add
+ * any non-standard modules and perform other setup tasks prior to the
+ * engine start up.
+ *
+ * @param engine The game engine instance.
+ */
+bool GameFactory::SetupEngine(IGameEngine& engine) {
+
+    // Add mouse and keyboard module here
+    SDLInput* input = new SDLInput();
+    engine.AddModule(*input);
+
+    // Add Statistics module
+    engine.AddModule(*(new OpenEngine::Utils::Statistics(1000)));
+
+    // Create a root scene node
+    SceneNode* scene = new SceneNode();
+
+    // Supply the scene to the renderer
+    this->renderer->SetSceneRoot(scene);
+
+    // Add RenderStateNode to change rendering features at runtime
+    RenderStateNode* rNode = new RenderStateNode();
+    rNode->AddOptions(RenderStateNode::RENDER_TEXTURES);
+    rNode->AddOptions(RenderStateNode::RENDER_SHADERS);
+    scene->AddNode(rNode);
+    
+    // Bind keys for changing rendering state
+    RenderStateHandler* renderStateHandler = new RenderStateHandler(rNode);
+    renderStateHandler->BindToEventSystem();
+
+    // Bind the quit handler
+    QuitHandler* quit_h = new QuitHandler();
+    quit_h->BindToEventSystem();
+
+    // Bind the camera to the viewport
+    FollowCamera* camera = new FollowCamera( *(new InterpolatedViewingVolume( *(new ViewingVolume()) )));
+    Frustum* frustum = new Frustum(*camera, 20, 5000);
+    viewport->SetViewingVolume(frustum);
+
+    // Register movement handler to be able to move the camera
+    MoveHandler* move = new MoveHandler(*camera);
+    move->BindToEventSystem();
+    engine.AddModule(*move);
+    
+    // set the resources directory
+    string resources = "projects/OERacer/data/";
+    ResourceManager::SetPath(resources);
+    logger.info << "Resource directory: " << resources << logger.end;
+
+    // load the resource plug-ins
+    ResourceManager::AddModelPlugin(new OBJPlugin());
+    ResourceManager::AddTexturePlugin(new TGAPlugin());
+    ResourceManager::AddShaderPlugin(new GLSLPlugin());
+    
+    // pointer to Box transformation node
+    RigidBox* box = NULL;
+    Vector<3,float> position(2, 100, 2);
+
+    // Add models from models.txt to the scene
+    ISceneNode* current = rNode;
+    ISceneNode* dynamicObjects = new SceneNode();
+    ISceneNode* staticObjects  = new SceneNode();
+    ISceneNode* physicObjects  = new SceneNode();
+    
+    // Open the models file
+    ifstream* mfile = File::Open("projects/OERacer/models.txt");
+    
+    bool dynamic = false;
+    while (!mfile->eof()) {
+        string mod_str;
+        getline(*mfile, mod_str);
+
+        // Check the string
+        if (mod_str[0] == '#' || mod_str == "") continue;
+        
+        // switch to static elements
+        if (mod_str == "dynamic") {
+            dynamic = true;
+            current = dynamicObjects;
+            continue;
+        }
+        else if (mod_str == "static") {
+            dynamic = false;
+            current = staticObjects;
+            continue;
+        }
+        else if (mod_str == "physic") {
+            dynamic = false;
+            current = physicObjects;
+            continue;
+        }
+        
+        // Load the model
+        IModelResourcePtr mod_res = ResourceManager::CreateModel(mod_str);
+        mod_res->Load();
+        if( mod_res->GetFaceSet() == NULL ) continue;
+
+        GeometryNode* mod_node = new GeometryNode();
+        mod_node->SetFaceSet(mod_res->GetFaceSet());
+        mod_res->Unload();
+
+        TransformationNode* mod_tran = new TransformationNode();
+        mod_tran->AddNode(mod_node);
+        if (dynamic) {
+            // Load ridget-box
+            box = new RigidBox( Box(*(mod_node->GetFaceSet())) );
+            box->SetCenter( position );
+            box->SetTransformationNode(mod_tran);
+
+            camera->SetPosition(position + Vector<3,float>(-150,40,0));
+            camera->LookAt(position);
+            camera->Follow(mod_tran);
+        }
+        current->AddNode(mod_tran);
+
+        // add the node to the move handler
+        move->nodes.push_back(mod_tran);
+        logger.info << "Successfully loaded " << mod_str << logger.end;
+    }
+    mfile->close();
+    delete mfile;
+    
+    rNode->AddNode(dynamicObjects);
+        
+    // Process static tree
+    logger.info << "Preprocessing of physics tree: started" << logger.end;
+    QuadBSPBuilder quadbsp;
+    QuadNode* hybridTreeRoot = quadbsp.Build(*physicObjects);
+    FixedTimeStepPhysics* physic = new FixedTimeStepPhysics( hybridTreeRoot );
+    logger.info << "Preprocessing of physics tree: done" << logger.end;
+
+    // Add FixedTimeStepPhysics module
+    physic->AddRigidBody(box);
+    engine.AddModule(*physic, IGameEngine::TICK_DEPENDENT);
+
+    logger.info << "Preprocessing of static tree: started" << logger.end;
+    QuadTreeBuilder qtb(5000,100);
+    QuadNode* qn = qtb.Build(*staticObjects);
+    rNode->AddNode(qn);
+    logger.info << "Preprocessing of static tree: done" << logger.end;
+
+    // Visualization of the frustum
+    //frustum->VisualizeClipping(true);
+    //rNode->AddNode(frustum->GetFrustumNode());
+
+    // add the RigidBox to the scene
+    if (box != NULL)
+    rNode->AddNode( box->GetRigidBoxNode() );
+    
+    // Keyboard bindings to the rigid box and camera
+    KeyboardHandler* keyHandler = new KeyboardHandler(camera,box,physic);
+    keyHandler->BindToEventSystem();
+    engine.AddModule(*keyHandler);
+
+    // Return true to signal success.
+    return true;
+}
+
+// Other factory methods. The returned objects are all created in the
+// factory constructor.
+IFrame*      GameFactory::GetFrame()    { return this->frame;    }
+IRenderer*   GameFactory::GetRenderer() { return this->renderer; }
---
+++ KeyboardHandler.h
@@ -0,0 +1,128 @@
+#ifndef _KEYBOARD_HANDLER_
+#define _KEYBOARD_HANDLER_
+
+#include <Display/Camera.h>
+#include <Physics/FixedTimeStepPhysics.h>
+#include <Physics/RigidBox.h>
+
+using namespace OpenEngine::Physics;
+
+class KeyboardHandler : public IModule {
+private:
+    bool up, down, left, right, mod;
+    float step;
+    Camera* camera;
+    RigidBox* box;
+    FixedTimeStepPhysics* physics;
+public:
+    KeyboardHandler(Camera* camera, RigidBox* box, FixedTimeStepPhysics* physics)
+        : up(false), down(false), left(false), right(false), camera(camera), box(box), physics(physics) {}
+
+    bool IsTypeOf(const std::type_info& inf) { return typeid(KeyboardHandler) == inf; }
+    void Initialize() {
+        step = 0.0f;
+    }
+    void Deinitialize() {}
+    void Process(const float deltaTime, const float percent) {
+        if( box == NULL ) return;
+        static float speed = 1750.0f;
+        static float turn = 550.0f;
+        Matrix<3,3,float> m; //orientation
+        if( up || down || left || right ) m = box->GetRotationMatrix();
+        // Forward 
+        if( up ){
+            Vector<3,float> dir = m.GetRow(0);
+            box->AddForce(dir * speed, 1);
+            box->AddForce(dir * speed, 2);
+            box->AddForce(dir * speed, 3);
+            box->AddForce(dir * speed, 4);
+        }
+        if( down ){
+            Vector<3,float> dir = -m.GetRow(0);
+            box->AddForce(dir * speed, 5);
+            box->AddForce(dir * speed, 6);
+            box->AddForce(dir * speed, 7);
+            box->AddForce(dir * speed, 8);
+        }
+        if( left ){
+            Vector<3,float> dir = -m.GetRow(2);
+            box->AddForce(dir * turn, 2);
+            box->AddForce(dir * turn, 4);
+        }
+        if( right ) {
+            Vector<3,float> dir = m.GetRow(2);
+            box->AddForce(dir * turn, 1);
+            box->AddForce(dir * turn, 3);
+        }
+    }
+
+    void KeyDown(KeyboardEventArg arg) {
+        switch ( arg.sym ) {
+        case KEY_r: {
+            physics->Initialize();
+            if( physics != NULL ){
+                if( box != NULL ) {
+                    box->ResetForces();
+                    box->SetCenter( Vector<3,float>(2, 1, 2) );
+                    logger.info << "Reset Physics" << logger.end;
+                }
+            }
+            break;
+        }
+
+        case KEY_SPACE:{
+            if( physics != NULL ){
+                physics->TogglePause();
+            }
+            break;
+        }
+        // Move the car forward
+        case KEY_UP:    up    = true; break;
+        case KEY_DOWN:  down  = true; break;
+        case KEY_LEFT:  left  = true; break;
+        case KEY_RIGHT: right = true; break;
+
+        // Log Camera position 
+        case KEY_c: {
+            Vector<3,float> camPos = camera->GetPosition();
+            logger.info << "Camera Position: " << camPos << logger.end;
+            break;
+        }
+
+        // Increase/decrease time in Physic
+        case KEY_PLUS:  mod = true; step =  0.001f; break;
+        case KEY_MINUS: mod = true; step = -0.001f; break;
+        
+
+        // Quit on Escape
+        case KEY_ESCAPE:
+            IGameEngine::Instance().Stop();
+            break;
+        default: break;
+        }
+    }
+
+    void KeyUp(KeyboardEventArg arg) {
+        switch ( arg.sym ) {
+        case KEY_UP:    up    = false; break;
+        case KEY_DOWN:  down  = false; break;
+        case KEY_LEFT:  left  = false; break;
+        case KEY_RIGHT: right = false; break;
+        case KEY_PLUS:  mod   = false; break;
+        case KEY_MINUS: mod   = false; break;
+
+        default: break;
+        }
+    }
+    
+    void BindToEventSystem() {
+        Listener<KeyboardHandler, KeyboardEventArg>* keyDown 
+            = new Listener<KeyboardHandler, KeyboardEventArg> (*this, &KeyboardHandler::KeyDown);
+        Listener<KeyboardHandler, KeyboardEventArg>* keyUp 
+            = new Listener<KeyboardHandler, KeyboardEventArg> (*this, &KeyboardHandler::KeyUp);
+        IKeyboard::keyDownEvent.Add(keyDown);
+        IKeyboard::keyUpEvent.Add(keyUp);
+    }
+};
+
+#endif
